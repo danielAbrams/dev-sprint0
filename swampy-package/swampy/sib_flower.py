c61630b0ef6b4cbd45528e8799cb0aabba50a7a6
danielAbrams <daniel@kitchenatpenn.com>
2014-03-30 14:59:17 -0400

    The Second Week

--- swampy-package/swampy/sib_flower.py
+++ swampy-package/swampy/sib_flower.py
@@ -6,17 +6,49 @@
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
-
+import math
 
 
 # This is where you put code to move bob
+def polyline (t, n, length, angle):
+	for i in range(n):
+		fd(t, length)
+		lt(t, angle)
+
+def arc (t, r, angle):
+	arc_length = 2 * math.pi * r * angle/360
+	n = int (arc_length /3)+1
+	step_length = arc_length/n
+	step_angle = float(angle) / n
+	polyline(t, n, step_length, step_angle)
 
 
 
+def petal (t, r, angle):
+	for i in range (2):
+		arc(t, r, angle)
+		lt (t, 180-angle)
 
+def flower(t, n, r, angle):
+	for i in range (n):
+		petal (t, r, angle)
+		lt(t, 360.0/n)
 
+pu(bob)
+fd(bob, -100)
+pd(bob)
+flower (bob, 7, 60.0, 60.0)
 
+pu(bob)
+fd(bob, 100)
+pd(bob)
 
+flower(bob, 10, 40.0, 80.0)
 
+pu(bob)
+fd(bob, 100)
+pd(bob)
+
+flower(bob, 20, 140.0, 20.0)
 wait_for_user()					
 
--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -11,10 +11,37 @@ bob = Turtle()
 
 # This is where you put code to move bob
 
+def polygon(turt, lengthturt, n):
+	form(turt, lengthturt, 360, n)
+
+#polygon(bob, 25, 4)
+#polygon(bob, 25, 5)
+#polygon(bob, 25, 6)
+#polygon(bob, 25, 7)
+#polygon(bob, 25, 8)
+#polygon(bob, 25, 9)
+
+def circle (turt, radius):
+	form(turt, radius, 360, 360)
+	
 
 
+def arc (turt, radius, theta):
+	form(turt, radius, theta, 360)
+	
+
+def form (turt, radius, theta, shape):
+	import math
+	x = 2.0*radius*radius*math.pi
+	print x
+	for i in range(theta):
+		fd(turt, x/360.0)
+		lt(turt, 360.0/shape)
 
 
+polygon(bob, 40, 5)
+#arc(bob, 5, 90)
+#circle(bob, 5)
 
 
 
